b4489feb9a8af14c1fa548523520a433b3c106a1
zhangxin <minicoral@outlook.com>
2020-08-20 21:24:33 +0800

    first commit

---
+++ src/main/java/com/sinosoft/roomorderserver/RoomorderserverApplication.java
@@ -0,0 +1,13 @@
+package com.sinosoft.roomorderserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomorderserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomorderserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sinosoft/roomorderserver/RoomorderserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinosoft.roomorderserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomorderserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
